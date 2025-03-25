d1823b6bbf8d1377dc092887b8bb0733e3100f72
JimBobJoe36 <larydbom@gmail.com>
2025-03-25 15:06:36 -0500

    Logic time

--- Secure Files/main.py
+++ Secure Files/main.py
@@ -8,10 +8,17 @@ user = os.getlogin()
 downloads = os.path.join("C:\\Users", user, "Documents", "Sentry", "Secure Files", "trustedLogs", "downloads.txt")
 downloads_size = os.stat(downloads).st_size
 # Procedures
-
+def bootUp():
+    global reasons
+    global user
+    global downloads
+    global downloads_size
+    updateLog(reasons)
+    if downloads_size == 0:
+        save_downloads_filenames()
+    else:
+        print("Welcome back " + user + "!")
+        checkDownloads()
+        
 #Code
-updateLog(reasons)
-if downloads_size == 0:
-    save_downloads_filenames()
-else:
-    checkDownloads()
+
