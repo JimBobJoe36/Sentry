798c1f3bce04de1554109caaa0fc7b4f3102f6d1
JimBobJoe36 <larydbom@gmail.com>
2025-03-25 15:33:04 -0500

    Yays

--- Secure Files/main.py
+++ Secure Files/main.py
@@ -8,17 +8,12 @@ user = os.getlogin()
 downloads = os.path.join("C:\\Users", user, "Documents", "Sentry", "Secure Files", "trustedLogs", "downloads.txt")
 downloads_size = os.stat(downloads).st_size
 # Procedures
-def bootUp():
-    global reasons
-    global user
-    global downloads
-    global downloads_size
-    updateLog(reasons)
-    if downloads_size == 0:
-        save_downloads_filenames()
-    else:
-        print("Welcome back " + user + "!")
-        checkDownloads()
-        
-#Code
 
+
+# Boot-up
+updateLog(reasons)
+if downloads_size == 0:
+    save_downloads_filenames()
+else:
+    print("Welcome back " + user + "!")
+    checkDownloads()
