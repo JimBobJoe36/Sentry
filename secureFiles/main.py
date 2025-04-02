0bc209b174379ef15a829dd2adbe81d9f3920394
JimBobJoe36 <larydbom@gmail.com>
2025-04-02 10:22:46 -0500

    Whoops

--- secureFiles/main.py
+++ secureFiles/main.py
@@ -5,7 +5,7 @@ from time import sleep
 
 reasons = "Booted Up"
 user = os.getlogin()
-downloads = os.path.join("C:\\Users", user, "Documents", "Sentry", "Secure Files", "trustedLogs", "downloads.txt")
+downloads = os.path.join("C:\\Users", user, "Documents", "Sentry", "secureFiles", "trustedLogs", "downloads.txt")
 downloads_size = os.stat(downloads).st_size
 tick = random.randint(0, 100) / 100
 
